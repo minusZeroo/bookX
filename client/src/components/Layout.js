48ba5c6fc3540312b07130aed6d2300e2474a364
minusZeroo <pcijideani@gmail.com>
2023-05-08 14:50:43 +0200

    Layout things

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../layout.css'
 import {Link, useLocation} from "react-router-dom";
+import {useSelector} from "react-redux";
 
 const Layout = ({children}) => {
+
+    const [collapsed, setCollapsed] = useState(false);
+    const {user} = useSelector((state) => state.user);
 
     const location  = useLocation();
 
@@ -40,7 +44,7 @@ const Layout = ({children}) => {
     return (
         <div className='main'>
             <div className='d-flex layout'>
-                <div className='sidebar'>
+                <div className= 'sidebar'>
                     <div className='sidebar-header'>
                         <h1>BX</h1>
                     </div>
@@ -49,18 +53,23 @@ const Layout = ({children}) => {
                             const isActive = location.pathname === menu.path
                             return <div className={`d-flex menu-item ${isActive && 'active-menu-item'}`}>
                                 <i className={menu.icon}></i>
-                                <Link to={menu.path}>{menu.name}</Link>
+                                {!collapsed && <Link to={menu.path}>{menu.name}</Link>}
                             </div>
                         })}
                     </div>
                 </div>
                 <div className='content'>
                     <div className="header">
-                        <i className="ri-close-fill close-icon"></i>
+                        {collapsed ? <i className="ri-menu-2-fill header-action-icon" onClick={() => setCollapsed(false)}></i> :
+                            <i className="ri-close-fill header-action-icon" onClick={() => setCollapsed(true)}></i>}
+
+                        <div className='d-flex align-items-center px-4'>
+                            <i className='ri-notification-line header-action-icon px-3'></i>
+                            <Link className="anchor" to='/profile'>{user?.name}</Link>
+
+                        </div>
                     </div>
-                    <div className="body">
-                        {children}
-                    </div>
+                    <div className="body">{children}</div>
                 </div>
             </div>
         </div>
--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -1,6 +1,38 @@
-import { Navigate } from "react-router";
+import {Navigate} from "react-router";
+import {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import axios from "axios";
+import {useNavigate} from "react-router-dom";
+import {setUser} from "../redux/userSlice";
 
 function ProtectedRoute(props) {
+    const { user } = useSelector((state) => state.user);
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const getUser = async() => {
+        try{
+            const response = await axios.post('/api/user/get-user-info-by-id', {token : localStorage.getItem('token')}, {
+                headers: {
+                    Authorization: `Bearer ${localStorage.getItem('token')}`,
+                }
+            });
+
+            if (response.data.success){
+                dispatch(setUser(response.data.data));
+            } else {
+                navigate("/login");
+            }
+        } catch (error) {
+            navigate("/login");
+        }        
+    }
+
+    useEffect(() => {
+        if (!user){
+            getUser();
+        }
+    }, [user]);
+
     if (localStorage.getItem("token")) {
         return props.children;
     } else {
